1244fd61691ad0dedf515877b1ba3001a3d02fd2
tamilan3 <tamilaninbathtamilan@gmail.com>
2022-07-23 09:49:38 +0530

    my changes

--- vidio/settings.py
+++ vidio/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
